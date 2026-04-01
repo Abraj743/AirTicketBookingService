da78033ad4888485752cc1778703a4630f793cd6
Abhay Raj Singh <abraj.singh743@gmail.com>
2026-04-01 18:36:44 +0530

    Added some logic related to pub sub of rabbit mq

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -12,8 +12,16 @@ class BookingController {
 
  async sendMessageToQueue (req,res) {
      const channel = await createChannel();
-     const data = {message:'Success'}
-     publishMessage(channel,REMINDER_BINDING_KEY,JSON.stringify(data));
+     const payload = {
+      data:{
+        subject:'This is a noti from queue',
+        content:'Some queue will subscribe this',
+        recepientEmail:'abraj.singh743@gmail.com',
+        notificationTime:'2026-03-31T09:49:00'
+      },
+      service:'CREATE_TICKET'
+     }
+     publishMessage(channel,REMINDER_BINDING_KEY,JSON.stringify(payload));
      return res.status(200).json({
         message:'Successfully published the event'
      });
